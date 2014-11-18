5a9d804260579b6b347409e43daa84c95282066e
Patrick Beeson <patrickbeeson@gmail.com>
2014-11-18 07:24:36 -0500

    Add initial structure for sitebuilder project .

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,24 +1,20 @@
-import sys
-import os
 import hashlib
+import os
+import sys
+
 from io import BytesIO
 from PIL import Image, ImageDraw
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.conf.urls import url
-from django.core.urlresolvers import reverse
-from django.core.cache import cache
-from django.core.wsgi import get_wsgi_application
+
 from django.conf import settings
-from django import forms
-from django.views.decorators.http import etag
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '&&oakrv_g%+@!7m2)u4pue)2gp-qs6j=g=4wx6zj_-dkw@$o3=')
+SECRET_KEY = os.environ.get('SECRET_KEY',
+    '&&oakrv_g%+@!7m2)u4pue)2gp-qs6j=g=4wx6zj_-dkw@$o3=')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 BASE_DIR = os.path.dirname(__file__)
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(', ')
 
 settings.configure(
     DEBUG=DEBUG,
@@ -28,7 +24,7 @@ settings.configure(
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware'
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
     INSTALLED_APPS=(
         'django.contrib.staticfiles',
@@ -36,21 +32,30 @@ settings.configure(
     TEMPLATE_DIRS=(
         os.path.join(BASE_DIR, 'templates'),
     ),
-    STATIC_FILES=(
+    STATICFILES_DIRS=(
         os.path.join(BASE_DIR, 'static'),
     ),
     STATIC_URL='/static/',
 )
 
+from django import forms
+from django.conf.urls import url
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
+from django.views.decorators.http import etag
+
 
 class ImageForm(forms.Form):
-    """ Form to validate requested placeholder image. """
+    """Form to validate requested placeholder image."""
 
     height = forms.IntegerField(min_value=1, max_value=2000)
-    width = form.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
 
     def generate(self, image_format='PNG'):
-        """ Generate an image of the given type and return as raw bytes. """
+        """Generate an image of the given type and return as raw bytes."""
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
         key = '{}.{}.{}'.format(width, height, image_format)
@@ -58,7 +63,7 @@ class ImageForm(forms.Form):
         if content is None:
             image = Image.new('RGB', (width, height))
             draw = ImageDraw.Draw(image)
-            text = '{} x {}'.format(width, height)
+            text = '{} X {}'.format(width, height)
             textwidth, textheight = draw.textsize(text)
             if textwidth < width and textheight < height:
                 texttop = (height - textheight) // 2
@@ -78,30 +83,31 @@ def generate_etag(request, width, height):
 
 @etag(generate_etag)
 def placeholder(request, width, height):
-    form = ImageForm(
-        {'height': height, 'width': width}
-    )
+    form = ImageForm({'height': height, 'width': width})
     if form.is_valid():
         image = form.generate()
         return HttpResponse(image, content_type='image/png')
     else:
-        return HttpResponseBadRequest('Invalid image request')
+        return HttpResponseBadRequest('Invalid Image Request')
 
 
 def index(request):
-    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    example = reverse('placeholder', kwargs={'width': 50, 'height':50})
     context = {
         'example': request.build_absolute_uri(example)
     }
     return render(request, 'home.html', context)
 
+
 urlpatterns = (
     url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder,
         name='placeholder'),
-    url(r'^$', index),
+    url(r'^$', index, name='homepage'),
 )
 
+
 application = get_wsgi_application()
+
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
