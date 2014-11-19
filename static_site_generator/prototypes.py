ad7e4fd2f4ad7e0cad41e65fe4b390fae4ad87ba
Patrick Beeson <patrickbeeson@gmail.com>
2014-11-19 08:10:46 -0500

    Reorg files, add views and urls and pages directory.

---
+++ static_site_generator/prototypes.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='f^mn^vql%!1q)1nmdj*59)z5lj2d9@8)u%3(j-47q5-#3q)px^',
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'django.contrib.webdesign',
+        'sitebuilder'
+    ),
+    STATIC_URL='/static/',
+    STATIC_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
